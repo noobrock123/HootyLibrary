018127b1601863a518c9517f6dec6c8bccfcf30f
noobrock123 <garfiwgame@hotmail.com>
2022-10-24 21:38:32 +0700

    Add buttons functionality in homepage and about

--- register/urls.py
+++ register/urls.py
@@ -2,6 +2,5 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('', views.register, name='register'),
-    #path('register/', views.register, name='register')
 
 ]
