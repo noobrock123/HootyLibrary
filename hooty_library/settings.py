3822d00a9ff5eec1be846b85bdd7a5fa90de8fb6
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-10-25 04:23:21 +0700

    correct register change register template location correct sign_up.html add staticfile dirs add staticfiles to .gitignore

--- hooty_library/settings.py
+++ hooty_library/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'register' / 'static',
+]
 AUTH_USER_MODEL = 'database_models.User'
 
 # Default primary key field type
