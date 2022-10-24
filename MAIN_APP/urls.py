68903c93b5722d57a2c78e5e173eabc4ca4788be
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-10-25 05:26:21 +0700

    fix conflict

--- MAIN_APP/urls.py
+++ MAIN_APP/urls.py
@@ -1,15 +1,9 @@
 from django.urls import include, path
 from . import views
-<<<<<<< HEAD
 
 app_name = "MAIN_APP"
 urlpatterns = [
     path('', views.index, name='home'),
     path('registeration/', include('register.urls')),
     path('about/', views.about, name='about')
-=======
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('registeration/', include('register.urls'))
->>>>>>> 6071948 (Linking homepage and make static files)
 ]
--- register/urls.py
+++ register/urls.py
@@ -2,9 +2,5 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('', views.register, name='register'),
-<<<<<<< HEAD
-=======
-    #path('register/', views.register, name='register')
->>>>>>> 6071948 (Linking homepage and make static files)
 
 ]
