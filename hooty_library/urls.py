b0be68d630ef492138e180b76bd7614666e9c338
noobrock123 <garfiwgame@hotmail.com>
2022-10-24 18:59:41 +0700

    Create link to homepag and make static files

--- hooty_library/urls.py
+++ hooty_library/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('register.urls')),
+    #path('', include('register.urls')),
+    path('', include('MAIN_APP.urls')),
 ]
