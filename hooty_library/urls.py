d9e9e7769198056da7e00a711be3ccde6ff0846b
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-10-24 22:06:16 +0700

    update urls.py

--- hooty_library/urls.py
+++ hooty_library/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userProfile.urls')),
+    #path('', include('register.urls')),
     path('', include('MAIN_APP.urls')),
 ]
