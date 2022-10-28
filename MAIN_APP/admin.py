cd0aceebb1328f2ba07385c5426279178fd5c294
6310682726 <69451907+6310682726@users.noreply.github.com>
2022-10-28 20:34:43 +0700

    Update admin.py

--- MAIN_APP/admin.py
+++ MAIN_APP/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Registered, Quota, Subject
+from database_models.models import User, CustomAccountManager
 
 
 admin.site.register(CustomAccountManager)
