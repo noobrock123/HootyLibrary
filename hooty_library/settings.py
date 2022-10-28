d9f706147a778ae745ec98647ccca69c58cdcb84
6310682726 <69451907+6310682726@users.noreply.github.com>
2022-10-28 21:20:31 +0700

    Update settings.py

--- hooty_library/settings.py
+++ hooty_library/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'database_models',
     'register.apps.RegisterConfig',
     'MAIN_APP.apps.MainAppConfig',
     'userProfile.apps.UserprofileConfig',
