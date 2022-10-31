816ebe51770f6a252ab140e07e5aedc208b0cad9
Vichayut Tubtim <6310682767@cn330>
2022-10-31 22:55:44 +0700

    Simplify views & add static in sign_im.html

--- MAIN_APP/views.py
+++ MAIN_APP/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import path, include
 
 # Create your views here.
+
 def index(request):
     return render(request, 'homepage/homepage.html')
 
