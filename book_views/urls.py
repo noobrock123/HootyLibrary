b9876847388aca4a700112a7970b4887c8bc58f4
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-11-01 09:20:08 +0700

    correct spelling

--- book_views/urls.py
+++ book_views/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('book/pdf/<str:book_id>', views.book_pdf, name='book_pdf'),
-    path('book/thumnail/<str:book_id>/',views.book_thumnail, name='book_thumnail'),
+    path('book/thumbnail/<str:book_id>/',views.book_thumbnail, name='book_thumbnail'),
     path('book/<str:book_id>', views.book_views, name='book')
 ]
