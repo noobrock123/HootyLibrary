1ca7e03ad89543fdc0a07961c430a433c36cbdf4
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-11-03 01:16:50 +0700

    provide url of media for development

--- hooty_library/urls.py
+++ hooty_library/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('userProfile.urls')),
@@ -26,3 +27,6 @@ urlpatterns = [
     path('',include('login_via_google.urls')),
     path('accounts/', include('allauth.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
