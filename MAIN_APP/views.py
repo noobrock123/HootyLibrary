2f7bb1bf7ddfd8ec6d00b5802a8f105c2854c38b
6310682726 <69451907+6310682726@users.noreply.github.com>
2022-11-03 07:48:08 +0700

    update search

--- MAIN_APP/views.py
+++ MAIN_APP/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import path, include
+from database_models.models import Book
 
 # Create your views here.
 
@@ -8,3 +9,17 @@ def index(request):
 
 def about(request):
     return render(request, 'about/about.html')
+
+def book(request):
+    return render(request, )
+
+def searchbar(request):
+    if request.method == "GET":
+        q = request.GET.get('q')
+        if q:
+            book = Book.objects.filter(book_name__icontains=q)
+            return render(request, 'homepage/homepage.html', {'book':book})
+        else:
+            book = Book.objects.all()
+            print("no information")
+            return render(request, 'homepage/homepage.html')
