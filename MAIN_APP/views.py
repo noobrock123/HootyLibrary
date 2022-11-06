66116ba1f148628cc33d305a7ebf7a869fcdae5f
Vichayut Tubtim <6310682767@cn330>
2022-11-06 22:54:10 +0700

    add views

--- MAIN_APP/views.py
+++ MAIN_APP/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.urls import path, include
 
 # Create your views here.
-
 def index(request):
     return render(request, 'homepage/homepage.html')
 
