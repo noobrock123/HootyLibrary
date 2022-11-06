54226889ac424ccc62d9a40970ce87fa67909411
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-11-06 09:48:23 +0700

    edit register view

--- register/views.py
+++ register/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from database_models.models import *
 from django.contrib.auth import login
-
+from django.contrib import messages
 from MAIN_APP import views as main_app
 # Create your views here.
 def register(request):
@@ -14,28 +14,13 @@ def register(request):
         email=request.POST.get('email')
         password=request.POST.get('password')
         confirm_password=request.POST.get('confirm_password')
-        gender=request.POST.get('gender')
-        age=request.POST.get('age')
-        occupation=request.POST.get('occupation')
-        bio=request.POST.get('bio')
-        social_link=request.POST.get('social_link')
-        donation_link=request.POST.get('donation_link')
-        is_staff=request.POST.get('is_staff')
-        is_active=request.POST.get('is_active')
-        profile_pic=request.POST.get('profile_pic')
+        if password != confirm_password:
+            messages.error('password do not match')
+            return render(request, 'register/templates/sign_up_and_in/sign_up.html', {})
         user = User.objects.create_user(
             username=username,
             email=email,
             password=password,
-            gender=gender,
-            age=age,
-            occupation=occupation,
-            bio=bio,
-            social_link=social_link,
-            donation_link=donation_link,
-            is_staff=is_staff if is_staff else User._meta.get_field('is_staff').get_default(),
-            is_active=is_active if is_active else User._meta.get_field('is_active').get_default(),
-            profile_pic=profile_pic,
         )
         login(request, user)
         return redirect('/')
