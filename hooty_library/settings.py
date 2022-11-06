8da10f17c32dc3d625af7312fbd686ac9ff5662d
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-11-06 10:23:45 +0700

    update sign up

--- hooty_library/settings.py
+++ hooty_library/settings.py
@@ -159,9 +159,9 @@ AUTHENTICATION_BACKENDS = (
  'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 5
+SITE_ID = 6
 LOGIN_REDIRECT_URL = '/'
-
+SOCIALACCOUNT_LOGIN_ON_GET=True
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
