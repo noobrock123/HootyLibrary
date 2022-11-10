660ad7295a66aae22b96b0bc50044d93251e541a
noobrock123 <garfiwgame@hotmail.com>
2022-11-10 13:02:25 +0700

    Re-design homepage and change some picture files

--- MAIN_APP/static/homepage/script.js
+++ MAIN_APP/static/homepage/script.js
@@ -44,3 +44,22 @@ const productContainers = [...document.querySelectorAll('.product-container')];
         function toggleFilter() {
             subFilter.classList.toggle("open-filter");
         }
+
+
+
+        const optionMenu = document.querySelector(".select-menu"),
+        selectBtn = optionMenu.querySelector(".select-btn"),
+        options = optionMenu.querySelectorAll(".option"),
+        sBtn_text = optionMenu.querySelector(".sBtn-text");
+ 
+ selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+ 
+ options.forEach(option =>{
+     option.addEventListener("click", ()=>{
+         let selectedOption = option.querySelector(".option-text").innerText;
+         sBtn_text.innerText = selectedOption;
+ 
+         optionMenu.classList.remove("active");
+     });
+ });
+ 
