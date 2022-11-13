bfbf8cbdb8e57f3552616e479a4f79855422a72e
noobrock123 <garfiwgame@hotmail.com>
2022-11-13 13:16:44 +0700

    Can create a book

--- book_views/urls.py
+++ book_views/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "book_views"
 urlpatterns = [
-    path('book/create_book/', views.create_book, name='create_book'),
+    path('book/create_book', views.create_book, name='create_book'),
     path('book/pdf/<str:book_id>', views.book_pdf, name='book_pdf'),
     path('book/thumbnail/<str:book_id>/',views.book_thumbnail, name='book_thumbnail'),
     path('book/<str:book_id>', views.book_views, name='book'),
