b3c731543b9b645b603777822270862a269f25c5
noobrock123 <garfiwgame@hotmail.com>
2022-11-14 09:28:39 +0700

    Fix current conflict

--- MAIN_APP/views.py
+++ MAIN_APP/views.py
@@ -1,10 +1,4 @@
-<<<<<<< Updated upstream
 from django.contrib.auth import logout
-=======
-from django.shortcuts import render, redirect
-from django.urls import path, include, reverse
-from database_models.models import Book, Read
->>>>>>> Stashed changes
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.template import loader
