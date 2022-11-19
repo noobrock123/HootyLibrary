8a12554cb053b69f090f9ac314d8e4c00161d836
Chaiwat Klanrueangsaeng <6310682742@cn331>
2022-11-20 02:10:18 +0700

    update test_genre

--- database_models/test_genre.py
+++ database_models/test_genre.py
@@ -10,7 +10,9 @@ class GenreTestCase(TestCase):
         self.genre1 = Genre.objects.create(
             genre_list='genre1'
         )
-
+    def test_genre_str(self):
+        # test genre __str__
+        self.assertEqual(f'{self.genre1.genre_list}', str(self.genre1))
     def test_genre_create_correct(self):
         # test genre was create correctly
         self.assertEqual(self.genre1.genre_list, 'genre1')
