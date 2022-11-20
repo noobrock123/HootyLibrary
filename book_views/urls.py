ae7f460edb31515b3c9712945636e5546f80287e
noobrock123 <garfiwgame@hotmail.com>
2022-11-21 00:18:32 +0700

    Fix bugs

--- book_views/urls.py
+++ book_views/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     #path('book/<str:book_id>/', views.book_views, name='book'),
     #path('book/<str:book_id>/review/', views.review, name='review'),
     #path('book/<str:book_id>/report/', views.report, name='report'),
-    #path('book/<str:book_id>/book_favorite/', views.book_favorite, name='book_favorite'),
+    path('<str:book_id>/book_favorite', views.book_favorite, name='book_favorite'),
     #path('book/<str:book_id>/issue/', views.issue, name='issue'),
     path('create_book', views.create_book, name='create_book'),
     path('pdf/<str:book_id>', views.book_pdf, name='book_pdf'),
     path('thumbnail/<str:book_id>/',views.book_thumbnail, name='book_thumbnail'),
-    path('<str:book_id>', views.book_views, name='book'),
+    path('<str:book_id>/', views.book_views, name='book'),
     path('<str:book_id>/', include('messaging.urls')),
     
 ]
