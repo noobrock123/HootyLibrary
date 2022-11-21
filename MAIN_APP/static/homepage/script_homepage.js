e92a68e35517e60e1b380468c43da7b5e7191e5f
noobrock123 <garfiwgame@hotmail.com>
2022-11-21 23:47:47 +0700

    Fix bug at script_homepage.js

--- MAIN_APP/static/homepage/script_homepage.js
+++ MAIN_APP/static/homepage/script_homepage.js
@@ -13,53 +13,59 @@ setInterval(function(){
 
 
 const productContainers = [...document.querySelectorAll('.product-container')];
-        const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-        const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-        productContainers.forEach((item, i) => {
-            let containerDimensions = item.getBoundingClientRect();
-            let containerWidth = containerDimensions.width;
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
-            nxtBtn[i].addEventListener('click', () => {
-                item.scrollLeft += containerWidth;
-            })
+    const nxtBtn = [...item.querySelectorAll('.nxt-btn')];
+    const preBtn = [...item.querySelectorAll('.pre-btn')];
 
-            preBtn[i].addEventListener('click', () => {
-                item.scrollLeft -= containerWidth;
-            })
-        })
+    console.log(nxtBtn)
+    console.log(preBtn)
+    if (nxtBtn.length != 0) {
+        nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+        });
+    }
+    if (preBtn.length != 0) {
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+        });
+    }
+});
 
-        let subMenu = document.getElementById("subMenu");
-        let subTab = document.getElementById("subTab");
-        let subFilter = document.getElementById("subFilter");
+let subMenu = document.getElementById("subMenu");
+let subTab = document.getElementById("subTab");
+let subFilter = document.getElementById("subFilter");
         
-        function toggleMenu() {
-            subMenu.classList.toggle("open-menu");
-        }
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+}
     
-        function toggleTab() {
-            subTab.classList.toggle("open-tab");
-        }
+function toggleTab() {
+    subTab.classList.toggle("open-tab");
+}
     
-        function toggleFilter() {
-            subFilter.classList.toggle("open-filter");
-        }
+function toggleFilter() {
+    subFilter.classList.toggle("open-filter");
+}
 
 
 
-        const optionMenu = document.querySelector(".select-menu"),
-        selectBtn = optionMenu.querySelector(".select-btn"),
-        options = optionMenu.querySelectorAll(".option"),
-        sBtn_text = optionMenu.querySelector(".sBtn-text");
+const optionMenu = document.querySelector(".select-menu"),
+selectBtn = optionMenu.querySelector(".select-btn"),
+options = optionMenu.querySelectorAll(".option"),
+sBtn_text = optionMenu.querySelector(".sBtn-text");
  
- selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
  
- options.forEach(option =>{
-     option.addEventListener("click", ()=>{
-         let selectedOption = option.querySelector(".option-text").innerText;
-         sBtn_text.innerText = selectedOption;
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
  
-         optionMenu.classList.remove("active");
-     });
+        optionMenu.classList.remove("active");
+    });
  });
  
