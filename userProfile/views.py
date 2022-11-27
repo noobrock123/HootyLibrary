65299b4717901a505ff779120ed755fa593440bc
noobrock123 <garfiwgame@hotmail.com>
2022-11-27 20:41:37 +0700

    Finish userProfile

--- userProfile/views.py
+++ userProfile/views.py
@@ -34,6 +34,7 @@ def userProfile(request, user_id):
         'user_id': user.user_id,
         'popular' : Book.objects.filter(author=user).annotate(num=Count('read')).order_by('-num')[:8],
         'alias_name':user.alias_name,
+        'user_id': user.user_id,
     }
     return render(request, 'userProfile/userProfile.html', context)
 
